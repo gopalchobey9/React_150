4b8e8220312fb6d7adcce03db00629ab8eefacbb
gopal chobey <100458189+gopalchobey9@users.noreply.github.com>
2023-12-26 09:19:45 +0530

    todo list

--- first-app/src/components/Gopal.js
+++ first-app/src/components/Gopal.js
@@ -8,6 +8,9 @@ const Gopal = () => {
   return (
     <div>
         <h1>{arr2[val]}</h1>
+       {
+         (val===3)? <img src="https://www.shutterstock.com/shutterstock/photos/2275269793/display_1500/stock-vector-google-popular-realistic-social-media-logotype-editorial-illustration-eps-2275269793.jpg" alt="" />:"nahi aai image"
+       }
         {
             todo.map((item,index)=>{
             return <li key={index}>My task is this {item}& position is {index}</li>
--- first-app/src/components/Person.js
+++ first-app/src/components/Person.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Person = (props) => {
+    let [name,setName] =useState(props.name)
+    // let namee = props.name;
+    function handleClick(){
+    //    let name = "anonymous";
+       setName("anonymous");
+       console.log(name);
+    }
+     
+
   return (
     <div>
-    <h1>{props.name}</h1>
+
+    <div onClick={handleClick} >i am a div donot click me </div>
+    {/* <h1>{props.name}</h1> */}
+    <h1>{name}</h1>
     <p>{props.age}</p>
     
     </div>
   )
 }
-
+ 
 export default Person
